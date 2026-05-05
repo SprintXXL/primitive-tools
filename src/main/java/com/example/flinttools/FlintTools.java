922c8fef71e858a4bdd4fff4dc6a78264b910e0e
SprintXXL <mspmain@pm.me>
2026-05-05 23:07:56 +0200

    Add recipes + tiny cosmetic stuff (mcmod etc)

--- src/main/java/com/example/flinttools/FlintTools.java
+++ src/main/java/com/example/flinttools/FlintTools.java
@@ -11,7 +11,7 @@ public class FlintTools
 {
     public static final String MODID = "flinttools";
     public static final String NAME = "Flint Tools";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.0";
 
     private static Logger logger;
 
