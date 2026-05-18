a37bb4d1bc10480c21183025142782843fdda82e
SprintXXL <mspmain@pm.me>
2026-05-18 07:21:04 +0200

    v0.4.0

--- src/main/java/com/SprintXXL/primitivetools/PrimitiveTools.java
+++ src/main/java/com/SprintXXL/primitivetools/PrimitiveTools.java
@@ -11,7 +11,7 @@ public class PrimitiveTools
 {
     public static final String MODID = "primitivetools";
     public static final String NAME = "Primitive Tools";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "0.4.0";
 
     private static Logger logger;
 
